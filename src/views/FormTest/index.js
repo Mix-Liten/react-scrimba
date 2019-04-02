f9de51b1a579142a4b4ec63250cef2b10dbe6ffc
ianchang <ian.chang@dev.cctech-support.com>
2019-04-02 11:31:07 +0800

    + FormTest + componrnts

--- src/views/FormTest/index.js
+++ src/views/FormTest/index.js
@@ -1,6 +1,7 @@
 import React, {
   Component
 } from "react"
+import FormComponent from '../../components/FormTest'
 
 class FormTest extends Component {
   constructor() {
@@ -36,114 +37,10 @@ class FormTest extends Component {
 
   render() {
     return (
-      <main>
-        <form>
-          <input 
-            name="firstName" 
-            value={this.state.firstName} 
-            onChange={this.handleChange} 
-            placeholder="First Name" 
-          />
-          <br />
-          
-          <input 
-            name="lastName" 
-            value={this.state.lastName}
-            onChange={this.handleChange} 
-            placeholder="Last Name" 
-          />
-          <br />
-          
-          <input 
-            name="age" 
-            value={this.state.age}
-            onChange={this.handleChange} 
-            placeholder="Age" 
-          />
-          <br />
-          
-          <label>
-            <input 
-              type="radio" 
-              name="gender"
-              value="male"
-              checked={this.state.gender === "male"}
-              onChange={this.handleChange}
-            /> Male
-          </label>
-          
-          <br />
-          
-          <label>
-            <input 
-              type="radio" 
-              name="gender"
-              value="female"
-              checked={this.state.gender === "female"}
-              onChange={this.handleChange}
-            /> Female
-          </label>
-          
-          <br />
-          
-          <select 
-            value={this.state.destination} 
-            name="destination" 
-            onChange={this.handleChange}
-          >
-            <option value="">-- Please Choose a destination --</option>
-            <option value="germany">Germany</option>
-            <option value="norway">Norway</option>
-            <option value="north pole">North Pole</option>
-            <option value="south pole">South Pole</option>
-          </select>
-          
-          <br />
-          
-          <label>
-            <input 
-              type="checkbox"
-              name="isVegan"
-              onChange={this.handleChange}
-              checked={this.state.isVegan}
-            /> Vegan?
-          </label>
-          <br />
-          
-          <label>
-            <input 
-              type="checkbox"
-              name="isKosher"
-              onChange={this.handleChange}
-              checked={this.state.isKosher}
-            /> Kosher?
-          </label>
-          <br />
-          
-          <label>
-            <input 
-              type="checkbox"
-              name="isLactoseFree"
-              onChange={this.handleChange}
-              checked={this.state.isLactoseFree}
-            /> Lactose Free?
-          </label>
-          <br />
-          
-          <button>Submit</button>
-        </form>
-        <hr />
-        <h2>Entered information:</h2>
-        <p>Your name: {this.state.firstName} {this.state.lastName}</p>
-        <p>Your age: {this.state.age}</p>
-        <p>Your gender: {this.state.gender}</p>
-        <p>Your destination: {this.state.destination}</p>
-        <p>Your dietary restrictions:</p>
-        
-        <p>Vegan: {this.state.isVegan ? "Yes" : "No"}</p>
-        <p>Kosher: {this.state.isKosher ? "Yes" : "No"}</p>
-        <p>Lactose Free: {this.state.isLactoseFree ? "Yes" : "No"}</p>
-    </main>
+      <FormComponent
+        handleChange={this.handleChange}
+        data={this.state}
+      />
     )
   }
 }
