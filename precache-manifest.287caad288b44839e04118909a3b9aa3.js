c447ea2ecfe323eb470610a51264d2935a43269d
Mix-Liten <mixliten@gmail.com>
2019-11-23 01:29:51 +0800

    Updates

---
+++ precache-manifest.287caad288b44839e04118909a3b9aa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4208ff3f5a24e0ce88351a64651ebe40",
+    "url": "/react-scrimba/index.html"
+  },
+  {
+    "revision": "3f29bf4bd53cb1895265",
+    "url": "/react-scrimba/static/css/main.7202fac2.chunk.css"
+  },
+  {
+    "revision": "7ba0ed1d26bc8b5da178",
+    "url": "/react-scrimba/static/js/2.1e31604f.chunk.js"
+  },
+  {
+    "revision": "3f29bf4bd53cb1895265",
+    "url": "/react-scrimba/static/js/main.30572612.chunk.js"
+  },
+  {
+    "revision": "17fb69ff6db0a934ee23",
+    "url": "/react-scrimba/static/js/runtime-main.c4dcb08f.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/react-scrimba/static/media/Trollface.be324d5b.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-scrimba/static/media/logo.ee7cd8ed.svg"
+  }
+]);
